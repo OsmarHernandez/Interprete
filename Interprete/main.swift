a75c405a7e542f0ccd26b3068317c65b3f4aef31
Osmar Hernández <OsmarHernandez@macbook-pro-de-osmar.local>
2016-10-08 15:34:24 -0600

    Initial Commit

---
+++ Interprete/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Interprete
+//
+//  Created by Osmar Hernández on 08/10/16.
+//  Copyright © 2016 Osmar Hernández. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
