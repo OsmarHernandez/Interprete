bc4577116be381eac966b591babb27b0b2a7733f
Osmar Hernández <OsmarHernandez@macbook-pro-de-osmar.local>
2016-10-08 18:43:01 -0600

    Inicio

--- Interprete/main.swift
+++ Interprete/main.swift
@@ -1,12 +1,50 @@
-//
-//  main.swift
-//  Interprete
-//
-//  Created by Osmar Hernández on 08/10/16.
-//  Copyright © 2016 Osmar Hernández. All rights reserved.
-//
+/**
+ * Interprete
+ *
+ *
+ */
 
 import Foundation
 
-print("Prueba...")
+/**
+ * Primera Parte : Calculadora
+ */
 
+let welcome: String = "**** Welcome to the first part of the interpreter, the Calculator ****"
+let example: String = "Introduce alguna operacion aritmetica. Ej. 3 + 5"
+var input: String = ""
+
+// Operacion generada por el usuario
+func userString() {
+    print(example)
+    return input = readLine()!
+}
+
+// Prueba
+func prueba() {
+    print("Usuario tecleo: \(input), Tamaño: \(input.characters.count)")
+}
+
+/**
+ * Separando String para dividirlo en Token (Lexer?)
+ */
+func Lexer(inputString input: String) {
+    // Arreglo de caracteres
+    var array = [Character]()
+    
+    // Descomponiendo el string para crear el arreglo que determinara los tokens
+    for character in input.characters {
+        array.append(character)
+    }
+}
+
+userString()
+prueba()
+Lexer(inputString: input)
+
+/**
+ * Predefiniendo Token. <Tipo, Dato> Ej: <INTEGER, 3>
+ */
+class Token {
+    
+}
