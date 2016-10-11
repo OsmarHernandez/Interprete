5b992baa19eff34a8f072adaa4e5c8c86413dbf4
Osmar Hernández <OsmarHernandez@macbook-pro-de-osmar.local>
2016-10-11 13:22:43 -0600

    Nuevo código

---
+++ Interprete/Tokens.swift
@@ -0,0 +1,67 @@
+//
+//  Tokens.swift
+//  Interpreter
+//
+//  Created by Osmar Hernández, Jesus Ruiz and Armando Minjares on 10/10/16.
+//  Copyright © 2016 Osmar Hernández. All rights reserved.
+//
+
+import Foundation
+
+let spaceRegex: String = "[ \t\n]"
+let numberRegex: String = "[0-9]+"
+let operandRegex: String = "[\\+\\-\\*\\/]"
+
+enum Token {
+    case Number(Int)
+    case Operand(String)
+}
+
+typealias TokenGenerator = (String) -> Token?
+
+let tokenList: [(String, TokenGenerator)] = [
+    (spaceRegex, {_ in nil}),
+    (numberRegex, {(intValue: String) in .Number((intValue as NSString).integerValue)}),
+    (operandRegex, {(stringValue: String) in .Operand(stringValue)})
+]
+
+var expressions = [String: NSRegularExpression]()
+public extension String {
+    public func match(regex: String) -> String? {
+        let expression: NSRegularExpression
+        if let exists = expressions[regex] {
+            expression = exists
+        } else {
+            expression = try! NSRegularExpression(pattern: "^\(regex)", options: [])
+            expressions[regex] = expression
+        }
+        
+        let range = expression.rangeOfFirstMatchInString(self, options: [], range: NSMakeRange(0, self.utf16.count))
+        if range.location != NSNotFound {
+            return (self as NSString).substringWithRange(range)
+        }
+        return nil
+    }
+}
+
+
+func Lexer(inputString input: String) -> [Token] {
+    var tokens: Array = Array<Token>()
+    var input: String = input
+
+    while (input.characters.count > 0) {
+        
+        for (pattern, generator) in tokenList {
+            if let theValue = input.match(pattern) {
+                if let aValue = generator(theValue) {
+                    tokens.append(aValue)
+                }
+                
+                input = input.substringFromIndex(input.startIndex.advancedBy(theValue.characters.count))
+                break
+            }
+        }
+    }
+    
+    return tokens
+}
--- Interprete/main.swift
+++ Interprete/main.swift
@@ -1,118 +1,25 @@
-/**
- * Interprete
- *
- *
- */
+ //
+ //  Tokens.swift
+ //  Interpreter
+ //
+ //  Created by Osmar Hernández, Jesus Ruiz and Armando Minjares on 10/8/16.
+ //  Copyright © 2016 Osmar Hernández. All rights reserved.
+ //
+ 
+ import Foundation
+ 
+ let welcome: String = "**** Welcome to the first part of the Interpreter: Calculator ****"
+ let example: String = "Write an arithmetic expression: \n Ex. 10 + 2 + 4"
+ var inputString: String?
 
-import Foundation
-
-/**
- * Primera Parte : Calculadora
- */
-
-let bienvenida: String = "**** Bienvenido a la primera parte del interprete: Calculadora ****"
-let ejemplo: String = "Introduce alguna operacion aritmetica.\n Ej. 3 + 5..."
-var entrada: String?
-
-func textoUsuario() {
-    print(ejemplo)
-    entrada = readLine()!
-}
-
-class Token {
-    var tipo: String    // ENTERO, OPERANDO, EOF (no utilizado)
-    var valor: String   // [0 - 9], [+ - * /]
+ func main() {
+    print(welcome)
+    print(example)
+    inputString = readLine()!
     
-    init(entradaTipo tipo: String, entradaValor valor: String) {
-        self.tipo = tipo
-        self.valor = valor
-    }
-}
-
-class Lexer {
-    var tokenValorA: Token?
-    var tokenValorB: Token?
-    var tokenValorC: Token?
+    let token = Lexer(inputString: inputString!)
     
-    func separandoToken(textoEntrada texto: String) {
-        var posicion: Int = 0
-        var firstToken: Bool = false
-        var thereIsValue: Bool = false
-
-        while posicion < texto.characters.count {
-            let inicioString = texto.startIndex
-            let finalString = inicioString.advancedBy(posicion)
-
-            let valorLeido:String = String(texto[finalString])
-            let valorActual: String = String(texto[finalString])
-            
-            if Int(valorLeido) != nil && firstToken == false {
-                if thereIsValue == false {
-                    tokenValorA = Token(entradaTipo: "ENTERO", entradaValor: valorActual)
-                    print("Token creado, valor: \(tokenValorA!.valor)")
-                    thereIsValue = true
-                    sleep(2)
-                } else if thereIsValue == true {
-                    tokenValorA!.valor += valorActual
-                    print("Token actualizado, valor: \(tokenValorA!.valor)")
-                    sleep(2)
-                }
-            } else if valorActual == "+" || valorActual == "-" || valorActual == "*" || valorActual == "/" {
-                tokenValorB = Token(entradaTipo: "OPERANDO", entradaValor: valorActual)
-                print("Token creado, valor: \(tokenValorB!.valor)")
-                firstToken = true
-                thereIsValue = false // CAMBIAR PARA NO CONFUNDIR
-                sleep(2)
-            } else if Int(valorLeido) != nil && firstToken == true {
-                if thereIsValue == false {
-                    tokenValorC = Token(entradaTipo: "OPERANDO", entradaValor: valorActual)
-                    print("Token creado, valor: \(tokenValorC!.valor)")
-                    thereIsValue = true
-                    sleep(2)
-                }
-                else if thereIsValue == true {
-                    tokenValorC!.valor += valorActual
-                    print("Token actualizado, valor: \(tokenValorC!.valor)")
-                    sleep(2)
-                }
-            }
-            
-            posicion += 1
-        }
-    }
-    
-    func operaciones(valorOperando operando: String) {
-        let valorX: Int? = Int(lexer.tokenValorA!.valor)!
-        let valorY: Int? = Int(lexer.tokenValorC!.valor)!
-        
-        var resultado: Int
-        
-        switch operando {
-        case "+":
-            resultado = valorX! + valorY!
-        case "-":
-            resultado = valorX! - valorY!
-        case "*":
-            resultado = valorX! * valorY!
-        case "/":
-            resultado = valorX! / valorY!
-        default:
-            resultado = 0
-        }
-        
-        print(resultado)
-    }
-}
-
-let lexer = Lexer()
-
-func main() {
-    print(bienvenida)
-    sleep(2)
-    textoUsuario()
-    print("Entrada: \(entrada!)")
-    lexer.separandoToken(textoEntrada: entrada!)
-    lexer.operaciones(valorOperando: lexer.tokenValorB!.valor)
-}
-
+    print("\(token)\n")
+ }
+ 
 main()
