946529273fc7099d5d3bf286a401fb43887a7a51
Osmar Hernández <OsmarHernandez@macbook-pro-de-osmar.local>
2016-10-13 12:51:20 -0600

    Pequeña mejora

--- Interprete/Operations.swift
+++ Interprete/Operations.swift
@@ -9,54 +9,24 @@
 import Foundation
 
 indirect enum BinaryTree<Token> {
-    case Node(BinaryTree<Token>, String, BinaryTree<Token>)
+    case Node(BinaryTree<Token>, Token, BinaryTree<Token>)
     case Leaf
 }
 
 func generateValues(Tokens : [Token]) -> [BinaryTree<Token>] {
     var values: Array = Array<BinaryTree<Token>>()
-    
-    for tokens in Tokens {
-        switch tokens {
-        case let .Number(tokens):
-            values.append(BinaryTree.Node(.Leaf, String(tokens), .Leaf))
-        case let .Operand(tokens):
-                                        // token[-1], token, token[+1]
-            values.append(BinaryTree.Node(.Leaf, tokens, .Leaf))
+ 
+    for i in Tokens.startIndex...Tokens.endIndex-1 {
+        switch Tokens[i] {
+        case .Number:
+            values.append(BinaryTree.Node(.Leaf, Tokens[i], .Leaf))
+        case .Operand:
+            values.append(BinaryTree.Node(.Leaf, Tokens[i], .Leaf))
         default:
             print("No implementado")
         }
+        print(Tokens[i])
     }
-    
-    for i in 0...5 {
-        print(i + 1)
-    }
-    
-    /*
-    for element in array:
-        print(elemtn)
-    
-    for i in range(0, len(array))
-        print(array[i])
-    */
+
     return values
 }
-
-/*
-func test(Tokens : [Token]) -> [Token] {
-    for token in Tokens {
-        switch token {
-        case let .Operand(token):
-            print(token)
-        case let .Number(token):
-            print(token)
-        case let .ParenthOp(token):
-            print(token)
-        case let .ParenthCl(token):
-            print(token)
-        }
-    }
-    
-    return Tokens
-}
-*/
