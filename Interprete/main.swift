935a419f472e267bdd92d3c0f3e707eba20543c8
Osmar Hernández <OsmarHernandez@macbook-pro-de-osmar.local>
2016-10-14 17:24:20 -0600

    Desde cero

--- Interprete/main.swift
+++ Interprete/main.swift
@@ -7,23 +7,3 @@
  //
  
  import Foundation
- 
- let welcome: String = "**** Welcome to the first part of the Interpreter: Calculator ****"
- let example: String = "Write an arithmetic expression: \n Ex. 10 + 2 + 4"
- var inputString: String?
-
- func main() {
-    print(welcome)
-    print(example)
-    inputString = readLine()!
-  
-    let tokens = Lexer(inputString: inputString!)
-
-    let newValues = generateValues(tokens)
-
-    for values in newValues {
-        print(values)
-    }
- }
- 
-main()
